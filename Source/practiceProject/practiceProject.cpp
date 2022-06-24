e71fdc5db4314315516412268d0497755799f19c
unknown <psbsanno.com@gmail.com>
2022-06-24 15:12:06 +0900

    init

---
+++ Source/practiceProject/practiceProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "practiceProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, practiceProject, "practiceProject" );
